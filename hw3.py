a4efb1408afa1745ef08025009c69ed5fdbd5d69
mcfralish <mcfralish@gmail.com>
2022-02-21 23:26:12 -0500

    problem complete

--- hw3.py
+++ hw3.py
@@ -1,29 +1,51 @@
-import random
+# pyright: reportMissingImports=false,
 import numpy as np
+import csv
+
 
 def generate(k):
     tuples = []
     PID_counter = 1000
 
-    def gen_PID():
-        PID = PID_counter
-        PID_counter += 1
-        return PID
-
     def gen_cycles():
-        nparr = np.random.normal(loc = .5, size = 1)
-        return abs(Integer(nparr[0] * 10000 + 1000))
+        cycles = int(np.random.gamma(5, 2.0) * 500) + 1000
+
+        # Hard truncates at min/max
+        if cycles < 1000:
+            cycles = 1000
+        if cycles > 11000:
+            cycles = 11000
+
+        return cycles
+        
 
     def gen_mem():
-        nparr = np.random.normal(loc = .2, size = 1)
-        return Integer(nparr[0] * )
+        mem = int(np.random.gamma(2, 2.0) * 5)
+        
+        # Hard truncates at min/max
+        if mem < 0:
+            mem = 0
+        if mem > 100:
+            mem = 100
 
+        return mem
 
 
     for i in range(k):
-        new_tuple = (gen_PID(), gen_cycles(), gen_mem())
+        new_tuple = (PID_counter, gen_cycles(), gen_mem())
         tuples.append(new_tuple)
+        PID_counter += 1
 
     return tuples
 
-print(generate(100))
+tuples = generate(100)
+
+f = open('./tuples.csv', 'w')
+w = csv.writer(f)
+
+header = ['PID', 'Cycles', 'Memory in KB']
+w.writerow(header)
+for i in range(len(tuples)):
+    w.writerow(tuples[i])
+
+f.close()
